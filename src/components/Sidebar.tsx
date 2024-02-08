9d31851e074d27ff0b677674352c4c79551fde10
nn1a <nn1a@icloud.com>
2024-02-09 02:13:00 +0900

    update

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+
+const drawerWidth: number = 240;
+
+export const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+export const SideBarMenu = ({open}: any) => {
+  return (
+    <Sidebar collapsed={!open}>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
+  );
+};
---
+++ src/pages/Main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+
+const MainPage = () => {
+  return (
+    <Paper
+      sx={{
+        p: 2,
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+    >
+      Hello
+    </Paper>
+  );
+};
+
+export default MainPage;
